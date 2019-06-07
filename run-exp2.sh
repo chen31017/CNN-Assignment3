ab7c98661ce778184fcc5015e09e4a0da94d1795
Edan <Edan.zwick@gmail.com>
2019-06-07 06:44:50 -0700

    Update all

--- run-exp2.sh
+++ run-exp2.sh
@@ -3,6 +3,6 @@ pwd
 for L in {2,4,8,16}
 do
     python -m hw3.experiments run-exp \
-    --run-name exp1_1_K32_L${L} --bs-train 64 --epochs 100 --early-stopping 2 \
+    --run-name exp1_1_K64_L${L} --bs-train 64 --epochs 100 --early-stopping 2 \
     --filters-per-layer 32 --layers-per-block ${L} --pool-every 1 --hidden-dims 1024 100;
 done;
