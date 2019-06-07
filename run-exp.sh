a3496bdbc2a8cfa18ab4b7a6e79de703da3725e8
Edan <Edan.zwick@gmail.com>
2019-06-07 06:16:59 -0700

    Update all

--- run-exp.sh
+++ run-exp.sh
@@ -1,6 +1,6 @@
 cd ..
 pwd
-for L in {2,4,8,16}
+for L in {8,16}#{2,4,8,16}
 do
     python -m hw3.experiments run-exp \
     --run-name exp1_1_K32_L${L} --bs-train 64 --epochs 100 --early-stopping 2 \
