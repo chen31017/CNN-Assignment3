1b200c8f244add864084a743512ad7b14b9dad37
Edan <Edan.zwick@gmail.com>
2019-06-13 23:13:51 -0700

    Update all

--- training.py
+++ training.py
@@ -8,7 +8,171 @@ from torch.utils.data import DataLoader
 from typing import Callable, Any
 from pathlib import Path
 from helpers.train_results import BatchResult, EpochResult, FitResult
-from statistics import mean
+
+class Trainer_1(abc.ABC):
+    """
+    A class abstracting the various tasks of training models.
+    Provides methods at multiple levels of granularity:
+    - Multiple epochs (fit)
+    - Single epoch (train_epoch/test_epoch)
+    - Single batch (train_batch/test_batch)
+    """
+    def __init__(self, model, loss_fn, optimizer, device=None):
+        """
+        Initialize the trainer.
+        :param model: Instance of the model to train.
+        :param loss_fn: The loss function to evaluate with.
+        :param optimizer: The optimizer to train with.
+        :param device: torch.device to run training on (CPU or GPU).
+        """
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.device = device
+
+        if self.device:
+            model.to(self.device)
+
+    def fit(self, dl_train: DataLoader, dl_test: DataLoader,
+            num_epochs, checkpoints: str = None,
+            early_stopping: int = None,
+            print_every=1, **kw) -> FitResult:
+        """
+        Trains the model for multiple epochs with a given training set,
+        and calculates validation loss over a given validation set.
+        :param dl_train: Dataloader for the training set.
+        :param dl_test: Dataloader for the test set.
+        :param num_epochs: Number of epochs to train for.
+        :param checkpoints: Whether to save model to file every time the
+            test set accuracy improves. Should be a string containing a
+            filename without extension.
+        :param early_stopping: Whether to stop training early if there is no
+            test loss improvement for this number of epochs.
+        :param print_every: Print progress every this number of epochs.
+        :return: A FitResult object containing train and test losses per epoch.
+        """
+        actual_num_epochs = 0
+        train_loss, train_acc, test_loss, test_acc = [], [], [], []
+
+        best_acc = None
+        epochs_without_improvement = 0
+
+        for epoch in range(num_epochs):
+            verbose = False  # pass this to train/test_epoch.
+            if epoch % print_every == 0 or epoch == num_epochs-1:
+                verbose = True
+            self._print(f'--- EPOCH {epoch+1}/{num_epochs} ---', verbose)
+
+            # TODO: Train & evaluate for one epoch
+            # - Use the train/test_epoch methods.
+            # - Save losses and accuracies in the lists above.
+            # - Optional: Implement checkpoints. You can use torch.save() to
+            #   save the model to a file.
+            # - Optional: Implement early stopping. This is a very useful and
+            #   simple regularization technique that is highly recommended.
+            # ====== YOUR CODE: ======
+            raise NotImplementedError()
+            # ========================
+
+        return FitResult(actual_num_epochs,
+                         train_loss, train_acc, test_loss, test_acc)
+
+    def train_epoch(self, dl_train: DataLoader, **kw) -> EpochResult:
+        """
+        Train once over a training set (single epoch).
+        :param dl_train: DataLoader for the training set.
+        :param kw: Keyword args supported by _foreach_batch.
+        :return: An EpochResult for the epoch.
+        """
+        self.model.train(True)  # set train mode
+        return self._foreach_batch(dl_train, self.train_batch, **kw)
+
+    def test_epoch(self, dl_test: DataLoader, **kw) -> EpochResult:
+        """
+        Evaluate model once over a test set (single epoch).
+        :param dl_test: DataLoader for the test set.
+        :param kw: Keyword args supported by _foreach_batch.
+        :return: An EpochResult for the epoch.
+        """
+        self.model.train(False)  # set evaluation (test) mode
+        return self._foreach_batch(dl_test, self.test_batch, **kw)
+
+    @abc.abstractmethod
+    def train_batch(self, batch) -> BatchResult:
+        """
+        Runs a single batch forward through the model, calculates loss,
+        preforms back-propagation and uses the optimizer to update weights.
+        :param batch: A single batch of data  from a data loader (might
+            be a tuple of data and labels or anything else depending on
+            the underlying dataset.
+        :return: A BatchResult containing the value of the loss function and
+            the number of correctly classified samples in the batch.
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def test_batch(self, batch) -> BatchResult:
+        """
+        Runs a single batch forward through the model and calculates loss.
+        :param batch: A single batch of data  from a data loader (might
+            be a tuple of data and labels or anything else depending on
+            the underlying dataset.
+        :return: A BatchResult containing the value of the loss function and
+            the number of correctly classified samples in the batch.
+        """
+        raise NotImplementedError()
+
+    @staticmethod
+    def _print(message, verbose=True):
+        """ Simple wrapper around print to make it conditional """
+        if verbose:
+            print(message)
+
+    @staticmethod
+    def _foreach_batch(dl: DataLoader,
+                       forward_fn: Callable[[Any], BatchResult],
+                       verbose=True, max_batches=None) -> EpochResult:
+        """
+        Evaluates the given forward-function on batches from the given
+        dataloader, and prints progress along the way.
+        """
+        losses = []
+        num_correct = 0
+        num_samples = len(dl.sampler)
+        num_batches = len(dl.batch_sampler)
+
+        if max_batches is not None:
+            if max_batches < num_batches:
+                num_batches = max_batches
+                num_samples = num_batches * dl.batch_size
+
+        if verbose:
+            pbar_file = sys.stdout
+        else:
+            pbar_file = open(os.devnull, 'w')
+
+        pbar_name = forward_fn.__name__
+        with tqdm.tqdm(desc=pbar_name, total=num_batches,
+                       file=pbar_file) as pbar:
+            dl_iter = iter(dl)
+            for batch_idx in range(num_batches):
+                data = next(dl_iter)
+                batch_res = forward_fn(data)
+
+                pbar.set_description(f'{pbar_name} ({batch_res.loss:.3f})')
+                pbar.update()
+
+                losses.append(batch_res.loss)
+                num_correct += batch_res.num_correct
+
+            avg_loss = sum(losses) / num_batches
+            accuracy = 100. * num_correct / num_samples
+            pbar.set_description(f'{pbar_name} '
+                                 f'(Avg. Loss {avg_loss:.3f}, '
+                                 f'Accuracy {accuracy:.1f})')
+
+        return EpochResult(losses=losses, accuracy=accuracy)
+
 
 class Trainer(abc.ABC):
     """
@@ -33,7 +197,6 @@ class Trainer(abc.ABC):
         self.optimizer = optimizer
         self.device = device
         model.to(self.device)
-        #print("not set to gpu")
 
     def fit(self, dl_train: DataLoader, dl_test: DataLoader,
             num_epochs, checkpoints: str = None,
@@ -86,22 +249,7 @@ class Trainer(abc.ABC):
             # - Implement early stopping. This is a very useful and
             #   simple regularization technique that is highly recommended.
             # ====== YOUR CODE: ======
-            if test_loss:
-                prev = test_loss[-1]
-            train_result = self.train_epoch(dl_train, verbose = verbose)
-            train_loss += [mean(train_result.losses)]
-            train_acc += [train_result.accuracy]
-            test_result = self.test_epoch(dl_test, verbose = verbose)
-            test_loss += [mean(test_result.losses)]
-            test_acc += [train_result.accuracy]
-            if epoch > 0 and (abs(test_loss[-1] - prev) <= 0.01): #arbitrary threshhold for improvment
-                epochs_without_improvement += 1
-            else:
-                epochs_without_improvement = 0
-            if early_stopping == epochs_without_improvement:
-                print("early stopping :(")
-                break
-            actual_num_epochs += 1
+            raise NotImplementedError()
             # ========================
 
             # Save model checkpoint if requested
@@ -296,11 +444,9 @@ class VAETrainer(Trainer):
         return BatchResult(loss.item(), 1/data_loss.item())
 
 
-
-class BlocksTrainer(Trainer):
+class BlocksTrainer(Trainer_1):
     def __init__(self, model, loss_fn, optimizer):
         super().__init__(model, loss_fn, optimizer)
-        self.i = 0
 
     def train_batch(self, batch) -> BatchResult:
         X, y = batch
@@ -311,13 +457,7 @@ class BlocksTrainer(Trainer):
         # - Optimize params
         # - Calculate number of correct predictions
         # ====== YOUR CODE: ======
-        res = self.model(X) #run fw pass
-        loss = self.loss_fn(res , y) #calc loss (also to prep grad)
-        grad = self.loss_fn.backward()  # calc grad through network
-        self.model.backward(grad)
-        self.optimizer.step() #optimize
-        _ , pred = res.max(dim=1) # when specifying dim, gives a tuple with index
-        num_correct = torch.eq(y,pred).sum()
+        raise NotImplementedError()
         # ========================
 
         return BatchResult(loss, num_correct)
@@ -329,16 +469,13 @@ class BlocksTrainer(Trainer):
         # - Forward pass
         # - Calculate number of correct predictions
         # ====== YOUR CODE: ======
-        res = self.model(X)
-        loss = self.loss_fn(res)
-        _, pred = res.max(dim=1)  # when specifying dim, gives a tuple with index
-        num_correct = torch.eq(y, pred).sum()
+        raise NotImplementedError()
         # ========================
 
         return BatchResult(loss, num_correct)
 
 
-class TorchTrainer(Trainer):
+class TorchTrainer(Trainer_1):
     def __init__(self, model, loss_fn, optimizer, device=None):
         super().__init__(model, loss_fn, optimizer, device)
 
@@ -354,15 +491,7 @@ class TorchTrainer(Trainer):
         # - Optimize params
         # - Calculate number of correct predictions
         # ====== YOUR CODE: ======
-        self.model.zero_grad()
-        res = self.model.forward(X) # run fw pass
-        loss = self.loss_fn(res, y) # calc loss (also to prep grad)
-        loss.backward()# calc grad through network
-        loss = loss.item()
-        self.optimizer.step() # optimize
-        #Done training, now report progress
-        _ , pred = res.max(dim=1) # when specifying dim, gives a tuple with index
-        num_correct = torch.eq(y,pred).sum().item()
+        raise NotImplementedError()
         # ========================
 
         return BatchResult(loss, num_correct)
@@ -378,10 +507,7 @@ class TorchTrainer(Trainer):
             # - Forward pass
             # - Calculate number of correct predictions
             # ====== YOUR CODE: ======
-            res = self.model.forward(X)
-            loss = self.loss_fn(res, y).item()
-            _, pred = res.max(dim=1)  # when specifying dim, gives a tuple with index
-            num_correct = torch.eq(y, pred).sum().item()
+            raise NotImplementedError()
             # ========================
 
         return BatchResult(loss, num_correct)
