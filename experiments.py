889d1339e14dd68aae3bcc2a752bd390772cf354
Edan <Edan.zwick@gmail.com>
2019-06-14 07:27:30 -0700

    exps

--- experiments.py
+++ experiments.py
@@ -22,7 +22,7 @@ DATA_DIR = os.path.join(os.getenv('HOME'), '.pytorch-datasets')
 def run_experiment(run_name, out_dir='./results', seed=None,
                    # Training params
                    bs_train=128, bs_test=None, batches=100, epochs=100,
-                   early_stopping=3, checkpoints=None, lr=5e-3, reg=1e-3,
+                   early_stopping=3, checkpoints=None, lr=1e-3, reg=1e-3,
                    # Model params
                    filters_per_layer=[64], layers_per_block=2, pool_every = 2,
                    hidden_dims=[1024], ycn=False,
@@ -66,9 +66,7 @@ def run_experiment(run_name, out_dir='./results', seed=None,
     for f in filters_per_layer:
         filters += ([f] * layers_per_block)
     #pool = pool_every * layers_per_block
-    pool = len(filters) // 4
-    if pool == 0:
-        pool = 1
+    pool = math.ciel(len(filters) / 3)
 
     #create model object
     model = models.ConvClassifier(in_size = in_size, out_classes = 10, filters = filters,
