0b665123da0cc93c3a8a1a37671c003c9a6c441a
Edan <Edan.zwick@gmail.com>
2019-06-14 00:30:38 -0700

    exps

--- experiments.py
+++ experiments.py
@@ -21,7 +21,7 @@ DATA_DIR = os.path.join(os.getenv('HOME'), '.pytorch-datasets')
 def run_experiment(run_name, out_dir='./results', seed=None,
                    # Training params
                    bs_train=128, bs_test=None, batches=100, epochs=100,
-                   early_stopping=3, checkpoints=None, lr=1e-3, reg=1e-3,
+                   early_stopping=3, checkpoints=None, lr=3e-3, reg=2e-3,
                    # Model params
                    filters_per_layer=[64], layers_per_block=2, pool_every=2,
                    hidden_dims=[1024], ycn=False,
