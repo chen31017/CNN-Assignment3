82ebd812dee71155b08f482fddb7f75de393e359
Edan <Edan.zwick@gmail.com>
2019-06-14 11:23:16 -0700

    exps

--- exp4.sh
+++ exp4.sh
@@ -7,6 +7,6 @@ do
         echo "running ${L} layers with ${k} filters per layer"
         echo "==============================================="
         python -m hw3.experiments run-exp \
-        --run-name exp1_2_L${L}_K64-128-256-512 --epochs 100 --early-stopping 2 --lr 1e-3 \
-        --filters-per-layer 64, 128, 256, 512 --layers-per-block ${L} --pool-every 2 --hidden-dims 1024;
+        --run-name exp2_L${L}_K64-128-256-512 --epochs 100 --early-stopping 2 --lr 1e-3 \
+        --filters-per-layer 64 128 256 512 --layers-per-block ${L} --pool-every 2 --hidden-dims 1024;
 done;
